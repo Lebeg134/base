976e166e2f956b8d4333d84111e1847412292d15
Lebeg134 <cactusmoovies@gmail.com>
2022-05-16 16:42:32 +0200

    imported jacoco, added mocking

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -17,8 +17,8 @@ public class TrainSensorTest {
     TrainController mock;
     @Before
     public void before() {
-         controllerDummy = new TrainController() {
-             int speed;
+        controllerDummy = new TrainController() {
+            int speed;
             @Override
             public void followSpeed() {
 
@@ -39,74 +39,45 @@ public class TrainSensorTest {
 
             }
         };
-        userDummy = new TrainUser() {
-            boolean alarm = false;
-            @Override
-            public int getJoystickPosition() {
-                return 0;
-            }
-
-            @Override
-            public boolean getAlarmFlag() {
-                return false;
-            }
-
-            @Override
-            public void overrideJoystickPosition(int joystickPosition) {
-
-            }
-
-            @Override
-            public boolean isAlarmState() {
-                return alarm;
-            }
-
-            @Override
-            public void setAlarmState(boolean alarmState) {
-                alarm = alarmState;
-            }
-        };
+        userDummy = mock(TrainUser.class);
         sensor = new TrainSensorImpl(controllerDummy, userDummy);
-        Assert.assertFalse(userDummy.isAlarmState());
-
-        mock = mock(TrainController.class);
     }
 
 
     @Test
     public void Nominal() { //Normal working conditions, no alarm should be fired. checks limit modification
         sensor.overrideSpeedLimit(250);
-        Assert.assertFalse(userDummy.isAlarmState());
+        verify(userDummy, times(0)).setAlarmState(true);
         Assert.assertEquals(250, sensor.getSpeedLimit());
         sensor.overrideSpeedLimit(200);
-        Assert.assertFalse(userDummy.isAlarmState());
+        verify(userDummy, times(0)).setAlarmState(true);
         Assert.assertEquals(200, sensor.getSpeedLimit());
     }
     @Test
     public void TopAlarm(){
         sensor.overrideSpeedLimit(499);
-        Assert.assertFalse(userDummy.isAlarmState());
+        verify(userDummy, times(0)).setAlarmState(true);
         Assert.assertEquals(499, sensor.getSpeedLimit());
         sensor.overrideSpeedLimit(501);
-        Assert.assertTrue(userDummy.isAlarmState());
+        verify(userDummy, times(1)).setAlarmState(true);
         Assert.assertEquals(501, sensor.getSpeedLimit());
     }
     @Test
     public void BottomAlarm(){
         sensor.overrideSpeedLimit(0);
-        Assert.assertFalse(userDummy.isAlarmState());
+        verify(userDummy, times(0)).setAlarmState(true);
         Assert.assertEquals(0, sensor.getSpeedLimit());
         sensor.overrideSpeedLimit(-1);
-        Assert.assertTrue(userDummy.isAlarmState());
+        verify(userDummy, times(1)).setAlarmState(true);
         Assert.assertEquals(-1, sensor.getSpeedLimit());
     }
     @Test
     public void DifferenceAlarm(){
         sensor.overrideSpeedLimit(499);
-        Assert.assertFalse(userDummy.isAlarmState());
+        verify(userDummy, times(0)).setAlarmState(true);
         Assert.assertEquals(499, sensor.getSpeedLimit());
         sensor.overrideSpeedLimit(1);
-        Assert.assertTrue(userDummy.isAlarmState());
+        verify(userDummy, times(1)).setAlarmState(true);
         Assert.assertEquals(1, sensor.getSpeedLimit());
 
     }
